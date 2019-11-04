f0ae4c52ee7d5633049c4df1a4e1f9fcd2c9a0a0
greatfullbon <pierro_kool@hotmail.com>
2019-11-04 09:32:27 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
  onmessage = function(e) {
   console.log('Worker: Message received from main script');
+  /*
   let result = e.data[0] * e.data[1];
   if (isNaN(result)) {
     postMessage('Please write two numbers');
@@ -8,6 +9,7 @@
     console.log('Worker: Posting message back to main script');
     postMessage(workerResult);
   }
+  */
 }
 
 
