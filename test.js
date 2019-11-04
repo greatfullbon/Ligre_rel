ce2e17a2056634884b75577062a2146db5248caf
greatfullbon <pierro_kool@hotmail.com>
2019-11-04 09:27:54 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-  function exportergraphxml()
+ onmessage = function(e) {
+  console.log('Worker: Message received from main script');
+  let result = e.data[0] * e.data[1];
+  if (isNaN(result)) {
+    postMessage('Please write two numbers');
+  } else {
+    let workerResult = 'Result: ' + result;
+    console.log('Worker: Posting message back to main script');
+    postMessage(workerResult);
+  }
+}
+
+
+function exportergraphxml()
 {
     
     var liste_utilisateur = [] ;                                       
